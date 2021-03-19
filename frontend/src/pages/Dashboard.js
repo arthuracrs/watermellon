69c90ddad9bfaa4739d9be6d32804d88bd68bca8
Arthur Calebe <arthuracrs3@gmail.com>
2021-03-19 13:47:28 +0000

    add secondary area to desktop screen on dashboard page

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -11,42 +11,45 @@ import ListUsers from '../components/ListUsers'
 import Header from '../components/Header'
 import CreatePost from '../components/CreatePost'
 import Profile from '../components/Profile'
+import Feed from '../components/Feed'
 
 import './Dashboard.css'
 
 function Dashboard() {
 
-    const [loggedUser, setLoggedUser] = useState('')
+    const [loggedUser, setLoggedUser] = useState({})
 
     function Content() {
         return (
             <Router>
-            <div className="dashboard">
-            <div className="dashboard-header">
-                <Header loggedUser={loggedUser} />
-            </div>
-            <div className="dashboard-main">
-                <Switch>
-                    <Route path="/" exact>
-                        <div>
-                            <CreatePost/>
-                            <h1>Outros Usuarios:</h1>
-                            <ListUsers/>
-                        </div>
-                    </Route>
-                    <Route path={"/:pathUsername"} exact>
-                        <Profile loggedUser={loggedUser}/>
-                    </Route>
-                    <Route path="*" >
-                        404
-                    </Route>
-                </Switch>
-            </div>
-            </div>
+                <div className="dashboard">
+                    <div className="dashboard-header">
+                        <Header loggedUser={loggedUser} />
+                    </div>
+                    <div className="dashboard-main">
+                        <Switch>
+                            <Route path="/" exact>
+                                <div>
+                                    <CreatePost/>
+                                    <Feed/>
+                                </div>
+                            </Route>
+                            <Route path={"/:pathUsername"} exact>
+                                <Profile loggedUser={loggedUser}/>
+                            </Route>
+                            <Route path="*" >
+                                404
+                            </Route>
+                        </Switch>
+                    </div>
+                    <div className="dashboard-secondary">
+                        <ListUsers/>
+                    </div>
+                </div>
             </Router>
         )
     }
-    return (<CheckAuth component={Content} saveUsername={setLoggedUser}/>)
+    return (<CheckAuth component={Content} saveUser={setLoggedUser}/>)
 }
 
 export default Dashboard
