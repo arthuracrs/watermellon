a7935c3cb4740631dfe2989fad047d021c6baa8b
EC2 Default User <ec2-user@ip-172-31-37-169.sa-east-1.compute.internal>
2021-03-28 21:26:38 +0000

    add search user page on mobile version

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,10 +13,13 @@ function Header(props) {
     return (
         <div className="header">
             <img src={Logo} alt="" className="header-logo"/>
-            <NavLink  exact to="/" activeClassName="selected" className="header-home-link">
+            <NavLink  exact to="/" activeClassName="selected" className="header-link">
                 <p> Home</p>
             </NavLink >
-            <NavLink  to={"/" + props.loggedUser.username} className="header-home-link" activeClassName="selected" >
+            <NavLink  to={"/user/search"} className="header-link search-link" activeClassName="selected" >
+                <p>Buscar</p>
+            </NavLink >
+            <NavLink  to={"/" + props.loggedUser.username} className="header-link" activeClassName="selected" >
                 <p>Perfil</p>
             </NavLink >
             <LogoutButton/>
