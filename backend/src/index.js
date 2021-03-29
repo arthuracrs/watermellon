c2bdf567449c66eba45fbf42f2d90d74b94dd954
EC2 Default User <ec2-user@ip-172-31-37-169.sa-east-1.compute.internal>
2021-03-29 13:08:20 +0000

    move database credentials to .env file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
 const mongoose = require("mongoose")
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
-mongoose.connect('mongodb+srv://thesupremebot:123@cluster0.hjizl.mongodb.net/teste?retryWrites=true&w=majority', {
+
+mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@cluster0.hjizl.mongodb.net/teste?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
